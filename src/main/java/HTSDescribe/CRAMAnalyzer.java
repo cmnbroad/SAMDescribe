7e9f896acdf8eb4967f67ad70486d25d4d0b3236
Chris Norman <cnorman@broadinstitute.org>
2020-07-09 15:15:48 -0400

    Update data size by series tracking.

--- src/main/java/HTSDescribe/CRAMAnalyzer.java
+++ src/main/java/HTSDescribe/CRAMAnalyzer.java
@@ -5,6 +5,7 @@ import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.samtools.cram.build.CramIO;
 import htsjdk.samtools.cram.encoding.CRAMEncoding;
 import htsjdk.samtools.cram.encoding.EncodingFactory;
+import htsjdk.samtools.cram.io.ITF8;
 import htsjdk.samtools.cram.structure.*;
 import htsjdk.samtools.cram.structure.block.Block;
 import htsjdk.samtools.seekablestream.SeekableFileStream;
@@ -13,6 +14,8 @@ import htsjdk.samtools.util.RuntimeIOException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,11 +29,13 @@ import java.util.Map;
  */
 public class CRAMAnalyzer extends HTSAnalyzer {
 
-    final Map<Integer, DataSeries> dataSeriesContentIDs = new HashMap<>();
-    final Map<DataSeries, Long> dataSeriesDataSizes = new HashMap<>();
-    final Map<Integer, Long> externalDataSizes = new HashMap<>();
+    final Map<DataSeries, Long> externalDataSeriesDataSizes = new HashMap<>();
+    final Map<Integer, Long> externalTagDataSizes = new HashMap<>();
+    long coreBlocksDataSize = 0L;
+    long containerHeaderSize = 0L;
+    long compressionHeaderSize = 0L;
+    long sliceHeaderSize = 0L;
     long recordCount = 0;
-    long coreBlocksSize = 0L;
 
     public CRAMAnalyzer(String fileName) {
         super(fileName);
@@ -40,12 +45,6 @@ public class CRAMAnalyzer extends HTSAnalyzer {
      * Run the analyzer for the file.
      */
     protected void doAnalysis() {
-        // initialize reverse lookup of DataSeries by contentID so we can track the distribution of
-        // data across allDataSeries
-        for (final DataSeries dataSeries: DataSeries.values()) {
-            dataSeriesContentIDs.put(dataSeries.getExternalBlockContentId(), dataSeries);
-        }
-
         int containerCount = 0;
         try (final SeekableFileStream seekableStream = new SeekableFileStream(new File(this.fileName))) {
             final CramHeader cramHeader = analyzeCRAMHeader(seekableStream);
@@ -53,7 +52,7 @@ public class CRAMAnalyzer extends HTSAnalyzer {
             while (!isEOF) {
                 long containerOffset = seekableStream.position();
                 final Container container = new Container(
-                        cramHeader.getVersion(),
+                        cramHeader.getCRAMVersion(),
                         seekableStream,
                         containerOffset);
                  isEOF = analyzeContainer(container, ++containerCount) ;
@@ -74,10 +73,10 @@ public class CRAMAnalyzer extends HTSAnalyzer {
     public CramHeader analyzeCRAMHeader(InputStream is) {
         final CramHeader cramHeader = CramIO.readCramHeader(is);
         emitln("\nCRAM File: " + fileName);
-        emitln("CRAM Version: " + cramHeader.getVersion().toString());
+        emitln("CRAM Version: " + cramHeader.getCRAMVersion().toString());
         emitln("CRAM ID Contents: " + String.format("%s", Base64.getEncoder().encodeToString(cramHeader.getId())));
 
-        final SAMFileHeader samHeader = Container.getSAMFileHeaderContainer(cramHeader.getVersion(), is, fileName);
+        final SAMFileHeader samHeader = Container.readSAMFileHeaderContainer(cramHeader.getCRAMVersion(), is, fileName);
         emitln("\n" + samHeader.toString());
         final SAMSequenceDictionary dict = samHeader.getSequenceDictionary();
         emitln(dict.toString());
@@ -97,19 +96,21 @@ public class CRAMAnalyzer extends HTSAnalyzer {
         if (container.isEOF()) {
             return true;
         }
-        analyzeCompressionHeader(container.getCompressionHeader());
+        // contentID to DataSeries (excludes tags ?)
+        final Map<Integer, DataSeries> dataSeriesByContentID = analyzeCompressionHeader(container.getCompressionHeader());
         int sliceCount = 0;
         for (final Slice slice : container.getSlices()) {
-            analyzeCRAMSlice(slice, ++sliceCount);
+            analyzeSlice(slice, ++sliceCount, dataSeriesByContentID);
         }
         return false;
     }
 
-    public void analyzeCompressionHeader(final CompressionHeader compressionHeader) {
+    public Map<Integer, DataSeries> analyzeCompressionHeader(final CompressionHeader compressionHeader) {
         //preservation map, data series encoding map, and tag encoding map
         analyzePreservationMap(compressionHeader);
-        analyzeDataSeriesEncodingMap(compressionHeader);
+        final Map<Integer, DataSeries> dataSeriesByContentID = analyzeDataSeriesEncodingMap(compressionHeader);
         analyzeTagEncodingMap(compressionHeader);
+        return dataSeriesByContentID;
     }
 
     public void analyzePreservationMap(final CompressionHeader compressionHeader) {
@@ -120,8 +121,14 @@ public class CRAMAnalyzer extends HTSAnalyzer {
                     compressionHeader.isAPDelta()));
     }
 
-    public void  analyzeDataSeriesEncodingMap(final CompressionHeader compressionHeader) {
-        emitln("\nData Series Encodings:");
+    public Map<Integer, DataSeries> analyzeDataSeriesEncodingMap(final CompressionHeader compressionHeader) {
+        // Since each container can in theory use a different set of ids for the data series in that
+        // container, we need to reconstruct the mapping for each container, and return it so that as
+        // we process Slice blocks and track data, we can calculate the data series to which we should
+        // accrue that block's data
+        final Map<Integer, DataSeries> dataSeriesByContentID = new HashMap<>();
+
+        emitln("\nData Series Encodings:\n");
         final CompressionHeaderEncodingMap encodingMap = compressionHeader.getEncodingMap();
         for (final DataSeries dataSeries: DataSeries.values()) {
             final EncodingDescriptor encodingDescriptor = encodingMap.getEncodingDescriptorForDataSeries(dataSeries);
@@ -131,6 +138,13 @@ public class CRAMAnalyzer extends HTSAnalyzer {
                                 dataSeries.getCanonicalName(),
                                 dataSeries.name())));
             } else {
+                // the encoding map has an encoding descriptor for this data series; determine if its external
+                // and if so, update the contentID to data series map so we can track how much data is used for
+                // the blocks for this data series
+                final Integer externalContentID = contentIDFromExternalDescriptor(dataSeries.getType(), encodingDescriptor);
+                if (externalContentID != null) {
+                    dataSeriesByContentID.put(externalContentID,  dataSeries);
+                }
                 emitln(String.format("%-50s %s",
                         String.format("DataSeries (%s/%s)",
                                 dataSeries.getCanonicalName(),
@@ -140,6 +154,7 @@ public class CRAMAnalyzer extends HTSAnalyzer {
                                 encodingDescriptor)));
             }
         }
+        return dataSeriesByContentID;
     }
 
     public void analyzeTagEncodingMap(final CompressionHeader compressionHeader) {
@@ -149,7 +164,7 @@ public class CRAMAnalyzer extends HTSAnalyzer {
             final EncodingDescriptor ep = entry.getValue();
             emitln(String.format("%-50s %s",
                     String.format("Content ID/Tag (%s/%s)",
-                            Integer.toString(contentID),
+                            contentID,
                             decomposeTagNameAndType(contentID)),
                      encodingDescriptorAsString(DataSeriesType.BYTE_ARRAY, ep)));
        }
@@ -159,7 +174,7 @@ public class CRAMAnalyzer extends HTSAnalyzer {
      * Display metadata for a CRAM container slice.
      *
      */
-    public void analyzeCRAMSlice(final Slice slice, final int sliceCount) {
+    public void analyzeSlice(final Slice slice, final int sliceCount, final Map<Integer, DataSeries> dataSeriesByContentID) {
         emitln(String.format("\n******Slice #: %d %s",
                 sliceCount,
                 slice.toString()));
@@ -169,30 +184,41 @@ public class CRAMAnalyzer extends HTSAnalyzer {
         emitln(String.format("%-50s %s",
                 "Core block ",
                 slice.getSliceBlocks().getCoreBlock()));
+
         if (slice.getEmbeddedReferenceContentID() != Slice.EMBEDDED_REFERENCE_ABSENT_CONTENT_ID) {
             emitln(String.format("Embedded reference block ID %d", slice.getEmbeddedReferenceContentID()));
         }
-        slice.getSliceBlocks().getExternalContentIDs().forEach((id) -> emitln(
-                String.format("%-50s %s",
-                        String.format("External Block (%s):", dataSeriesNameFromContentID(id)),
-                        slice.getSliceBlocks().getExternalBlock(id).toString())));
-        recordCount += slice.getNumberOfRecords();
 
-        updateDataDistribution(slice);
+        slice.getSliceBlocks().getExternalContentIDs().forEach((id) -> {
+            final Block block = slice.getSliceBlocks().getExternalBlock(id);
+            final String inflatedContent = block.getCompressedContentSize() >= block.getUncompressedContentSize() ?
+                    "***Inflated***" :
+                    "";
+            emitln(
+                    String.format("%s%-50s %s",
+                            inflatedContent,
+                            String.format("External Block (%s):", dataSeriesByContentID.get(id)),
+                            slice.getSliceBlocks().getExternalBlock(id).toString()));
+        });
+
+        updateDataDistribution(slice, dataSeriesByContentID);
+        recordCount += slice.getNumberOfRecords();
     }
 
-    final void updateDataDistribution(final Slice slice) {
-        coreBlocksSize += slice.getSliceBlocks().getCoreBlock().getCompressedContentSize();
-        for (final Integer contentID : slice.getSliceBlocks().getExternalContentIDs()) {
-            if (dataSeriesContentIDs.containsKey(contentID)) {
+    final void updateDataDistribution(final Slice slice, final Map<Integer, DataSeries> dataSeriesByContentID) {
+        final SliceBlocks sliceBlocks = slice.getSliceBlocks();
+
+        coreBlocksDataSize += sliceBlocks.getCoreBlock().getCompressedContentSize();
+        for (final Integer contentID : sliceBlocks.getExternalContentIDs()) {
+            if (dataSeriesByContentID.containsKey(contentID)) {
                 // if its a fixed DataSeries ID
-                dataSeriesDataSizes.merge(
-                        dataSeriesContentIDs.get(contentID),
-                        new Long(slice.getSliceBlocks().getExternalBlock(contentID).getCompressedContentSize()),
+                externalDataSeriesDataSizes.merge(
+                        dataSeriesByContentID.get(contentID),
+                        new Long(sliceBlocks.getExternalBlock(contentID).getCompressedContentSize()),
                         (oldValue, increment) -> oldValue + increment);
             } else {
                 // it must be a tag data series
-                externalDataSizes.merge(
+                externalTagDataSizes.merge(
                         contentID,
                         new Long(slice.getSliceBlocks().getExternalBlock(contentID).getCompressedContentSize()),
                         (oldValue, increment) -> oldValue + increment);
@@ -201,45 +227,54 @@ public class CRAMAnalyzer extends HTSAnalyzer {
     }
 
     public void emitDataDistribution() {
-        emitln("\nData Series Data Distribution (block resolution):");
-        emitln("Core Blocks: " + String.format("%,d", coreBlocksSize));
-        for (final DataSeries ds : DataSeries.values()) {
-            emitln(ds + " : " + String.format("%,d", dataSeriesDataSizes.get(ds)));
+        emitln("\nCore Block(s) Total: " + String.format("%,d\n", coreBlocksDataSize));
+        emitln("External Data Series Totals (external block resolution - all core data encodings accrue to the core block):\n");
+        for (final DataSeries ds : externalDataSeriesDataSizes.keySet()) {
+            emitln(String.format("%s: %,d", ds, externalDataSeriesDataSizes.get(ds)));
         }
 
-        emitln("\nTag Series Distribution:");
-        for (final Map.Entry<Integer, Long> externalEntry : externalDataSizes.entrySet()) {
-            if (externalEntry.getKey() != 0L) {
-                // external blocks should not have ID 0, but some implementations emit them...
-                final Integer contentID = externalEntry.getKey();
-                final String seriesName = contentID == 0L ? "ID 0" : decomposeTagNameAndType(externalEntry.getKey());
-                emitln( seriesName + " : " + String.format("%,d", externalEntry.getValue()));
-            }
+        emitln("\nTag Series Distribution:\n");
+        for (final Map.Entry<Integer, Long> externalEntry : externalTagDataSizes.entrySet()) {
+            final Integer contentID = externalEntry.getKey();
+            final String seriesName = contentID == 0L ? "ID 0" : decomposeTagNameAndType(externalEntry.getKey());
+            emitln(String.format("%s: %,d", seriesName, externalEntry.getValue()));
         }
     }
 
-    // Find a DataSeries that uses the provided content ID, otherwise return Block.NO_CONTENT_ID
-    private String dataSeriesNameFromContentID(final Integer contentID) {
-        if (contentID == Block.NO_CONTENT_ID) {
-            return String.format("NO_CONTENT_ID");
-        }
-        for (final DataSeries ds : DataSeries.values()) {
-            if (ds.getExternalBlockContentId() == contentID) {
-                return ds.name();
-            }
-        }
-        // if we don't find a data series that matches this block's content ID, then its
-        //a tag block, so decompose the contentID as a tag name
-        return String.format("%d/%s",
-                contentID,
-                decomposeTagNameAndType(contentID));
-    }
-
-    private String encodingDescriptorAsString(final DataSeriesType dsType, final EncodingDescriptor dscriptor) {
-        final String encodingIDString = EncodingID.values()[dscriptor.getEncodingID().getId()].toString();
+    private String encodingDescriptorAsString(final DataSeriesType dsType, final EncodingDescriptor descriptor) {
+        final String encodingIDString = EncodingID.values()[descriptor.getEncodingID().getId()].toString();
         final CRAMEncoding<?> encoding = EncodingFactory.createCRAMEncoding(
-                dsType, dscriptor.getEncodingID(), dscriptor.getEncodingParameters());
+                dsType, descriptor.getEncodingID(), descriptor.getEncodingParameters());
         return String.format("%s (%s)", encodingIDString, encoding.toString());
+    }
+
+    private Integer contentIDFromExternalDescriptor(final DataSeriesType dsType, final EncodingDescriptor descriptor) {
+        final CRAMEncoding<?> encoding = EncodingFactory.createCRAMEncoding(
+                dsType, descriptor.getEncodingID(), descriptor.getEncodingParameters());
+        switch (descriptor.getEncodingID()) {
+            case EXTERNAL:
+                return ITF8.readUnsignedITF8(encoding.toSerializedEncodingParams());
+
+            case BYTE_ARRAY_STOP:
+                final ByteBuffer buf = ByteBuffer.wrap(encoding.toSerializedEncodingParams());
+                buf.order(ByteOrder.LITTLE_ENDIAN);
+                final byte stopByte = buf.get(); // discard it
+                return ITF8.readUnsignedITF8(buf);
+
+            // Everything else is either not external, or is hybrid (BYTE_ARRAY_LEN), so we can't
+            // track the data for these at the block level since the data foes either partially or
+            // fully into the core block
+            case BYTE_ARRAY_LEN:
+            case NULL:
+            case GOLOMB:
+            case HUFFMAN:
+            case BETA:
+            case SUBEXPONENTIAL:
+            case GOLOMB_RICE:
+            case GAMMA:
+            default:
+                return null;
+        }
     }
 
     private String decomposeTagNameAndType(final int contentID) {
